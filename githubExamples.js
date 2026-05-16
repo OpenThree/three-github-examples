52c80b7704af38b9e121f1c517f33fefb4fefbdf
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-05-16 21:54:40 +0800

    sync projects

--- githubExamples.js
+++ githubExamples.js
@@ -5,8 +5,42 @@ export default [
         name_en: 'Recently Submitted',
         order: 0,
         children: [
-
-
+            {
+                id: 'LearningCell',
+                name: '细胞课堂',
+                name_en: 'LearningCell',
+                author: 'GordenSun',
+                openUrl: 'https://gordensun.github.io/LearningCell/',
+                githubUrl: 'https://github.com/GordenSun/LearningCell',
+                image: HOST + 'images/LearningCell.webp',
+            },
+            {
+                id: 'PSU-VR',
+                name: 'vr 视角',
+                name_en: 'PSU-VR',
+                author: 'andrewwoan',
+                openUrl: 'https://psu-vr.herokuapp.com/',
+                githubUrl: 'https://github.com/andrewwoan/PSU-VR',
+                image: HOST + 'images/PSU-VR.webp',
+            },
+            {
+                id: 'sooahs-room-folio',
+                name: '房间',
+                name_en: 'sooahs-room-folio',
+                author: 'andrewwoan',
+                openUrl: 'https://www.sooahs-room-folio.com/',
+                githubUrl: 'https://github.com/andrewwoan/sooahs-room-folio',
+                image: HOST + 'images/sooahs-room-folio.webp',
+            },
+            {
+                id: 'abigail-bloom-portolio-bokoko33',
+                name: 'abigail-bloom-portolio-bokoko33',
+                name_en: 'abigail-bloom-portolio-bokoko33',
+                author: 'andrewwoan',
+                openUrl: 'https://abigail-bloom-portolio-bokoko33.vercel.app/',
+                githubUrl: 'https://github.com/andrewwoan/abigail-bloom-portolio-bokoko33',
+                image: HOST + 'images/abigail-bloom-portolio-bokoko33.webp',
+            },
         ]
     }
 ]
