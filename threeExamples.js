09340dfdda980d279947ed8ce5f4cf965e93ebe0
zhangjingguang <2586300277@qq.com>
2026-06-22 14:58:38 +0800

    增加 three系列

---
+++ threeExamples.js
@@ -0,0 +1,18 @@
+const HOST = (window.origin + window.location.pathname).replace(/\/index.html$/, '/')
+
+export default [
+    {
+        pid: 'RecentlySubmitted',
+        name: '最近提交',
+        name_en: 'Recently Submitted',
+        order: 0,
+        children: [
+            {   
+                id: 'test',
+                name: 'test',
+                htmlUrl: HOST + 'three/test.html',
+                image: HOST + 'three/test.webp',
+            },
+        ]
+    }
+]
