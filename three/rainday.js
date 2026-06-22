7b0aa59fe6f9dc6883d50798195ba6f3415d98bf
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-22 16:55:56 +0800

    sync three 2026-06-22T08:55:54.634Z

--- three/rainday.js
+++ three/rainday.js
@@ -1,3 +1,7 @@
+import * as THREE from "three";
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 let scene,
 	camera,
 	renderer,
