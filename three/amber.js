38c62650903901f90f3866c57baf4eb52c7541b5
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-06-26 20:48:55 +0800

    sync three 2026-06-26T12:48:54.977Z

--- three/amber.js
+++ three/amber.js
@@ -1,4 +1,4 @@
-import * as THREE from "three/build/three.module.js";
+import * as THREE from "three";
     import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
     import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader.js";
     import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
