fc33b755b91e7e3f0db4c42d7c7508fded8d0d7f
Elegant At Every Time ----优雅永不过时 <2586300277@qq.com>
2026-07-28 17:27:16 +0800

    sync 2026-07-28T09:27:14.702Z

--- githubExamples.js
+++ githubExamples.js
@@ -934,6 +934,24 @@ export default [
                 githubUrl: 'https://github.com/achimala/TheLongSilence',
                 image: HOST + 'images/TheLongSilence.webp',
             },
+            {
+                id: 'vinyl',
+                name: 'vinyl',
+                name_en: 'vinyl',
+                author: 'AUTO',
+                openUrl: 'https://vinyl-rho-peach.vercel.app/',
+                githubUrl: 'https://github.com/thebuggeddev/vinyl',
+                image: HOST + 'images/vinyl.webp',
+            },
+            {
+                id: 'meteor3D',
+                name: '低代码 3D 场景可视化平台',
+                name_en: 'meteor3D',
+                author: 'AUTO',
+                openUrl: 'https://www.meteor3d.cn/',
+                githubUrl: 'https://github.com/nikonikoCW/Meteor3DEditor',
+                image: HOST + 'images/meteor3D.webp',
+            },
         ]
     }
 ]
